76f8388b7152b146ea8eb1dea2eebc11ff848b86
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-10-22 18:03:17 -0400

    untested

--- needthis.py
+++ needthis.py
@@ -1,30 +1,32 @@
-def encodeVer1(text): #for Encryption
+def encodeVer1(text, encryptionKey=8): #for Encryption
     """
     This is one of the ways that the text can be encrypted. It is very simple.
-    Syntax: encodeVer1(text)
+    parameter text: Decrypted (original) text
+    parameter encryptionKey: Encryption key (should be integer of length 1). For backwards compatibility purposes, defaults to 8.
     """
     encodedText = list(text)
     length = len(encodedText)
     for i in range(0,length):
         encodedText[i] = ord(encodedText[i])
     for i in range(0,length):
-        if encodedText[i] != 32:
-            encodedText[i] = encodedText[i] - 8
+        if encodedText[i] != 32
+            encodedText[i] = encodedText[i] - int(encryptionKey)[0]
     for i in range(0,length):
         encodedText[i] = str(chr(encodedText[i]))
     encodedText = ''.join(encodedText)
     return encodedText
-def decryptVer1(text): #for Decryption
+def decryptVer1(text, encryptionKey=8): #for Decryption
     """
     This is one of the ways text can be decrypted.
-    Syntax: decryptVer1(encrypted_text)
+    parameter text: Encrypted text
+    parameter encryptionKey: The encryption key (should be integer of length 1). For backwards compatibility purposes, defaults to 8.
     """
     decodedText = list(text)
     length = len(decodedText)
     for i in range(0,length):
         decodedText[i] = ord(decodedText[i])
         if decodedText[i] != 32:
-            decodedText[i] = decodedText[i] + 8
+            decodedText[i] = decodedText[i] + int(encryptionKey)[0]
         decodedText[i] = str(chr(decodedText[i]))
     decodedText = ''.join(decodedText)
     return decodedText
